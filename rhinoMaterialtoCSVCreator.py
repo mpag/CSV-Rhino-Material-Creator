9a146cb32f39953b37c334ad2c57e8c39d6277dd
mpag <mitchell.r.page@gmail.com>
2021-09-30 10:43:51 +1000

    Updates

--- rhinoMaterialtoCSVCreator.py
+++ rhinoMaterialtoCSVCreator.py
@@ -18,36 +18,34 @@ mats = sc.doc.Materials
 
 for material in mats:
     matTitle = Rhino.DocObjects.Material.Name.GetValue(material)
-    matName.append(matTitle)
-    
-    #Get Material Diffuse Colour
-    matDiff = Rhino.DocObjects.Material.DiffuseColor.GetValue(material)
-    matDiffCol = System.Drawing.ColorTranslator.ToString(matDiff)
-    matCol.append(matDiffCol)
-    
-    #Get Material Diffuse Bitmap Path
-    matTexBitmap = Rhino.DocObjects.Material.GetBitmapTexture(material)
-    if matTexBitmap is None:
-        matDiffPath.append("None")
-    else:
-        matTexBitmapPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexBitmap)
-        matDiffPath.append(matTexBitmapPath)
-    
-    #Get Material Bump Bitmap Path
-    matTexBump = Rhino.DocObjects.Material.GetBumpTexture(material)
-    if matTexBump is None:
-        matBumpPath.append("None")
-    else:
-        matTexBumpPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexBump)
-        matBumpPath.append(matTexBumpPath)
-    
-    #Get Material Diffuse Transparency Path
-    matTexTrans = Rhino.DocObjects.Material.GetTransparencyTexture(material)
-    if matTexTrans is None:
-        matTransPath.append("None")
-    else:
-        matTexTransPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexTrans)
-        matTransPath.append(matTexTransPath)
+    if matTitle is not None:
+        #Get Material Name
+        matName.append(matTitle)
+        #Get Material Diffuse Colour
+        matDiff = Rhino.DocObjects.Material.DiffuseColor.GetValue(material)
+        matDiffCol = System.Drawing.ColorTranslator.ToString(matDiff)
+        matCol.append(matDiffCol)
+        #Get Material Diffuse Bitmap Path
+        matTexBitmap = Rhino.DocObjects.Material.GetBitmapTexture(material)
+        if matTexBitmap is None:
+            matDiffPath.append('')
+        else:
+            matTexBitmapPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexBitmap)
+            matDiffPath.append(matTexBitmapPath)
+        #Get Material Bump Bitmap Path
+        matTexBump = Rhino.DocObjects.Material.GetBumpTexture(material)
+        if matTexBump is None:
+            matBumpPath.append('')
+        else:
+            matTexBumpPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexBump)
+            matBumpPath.append(matTexBumpPath)
+        #Get Material Diffuse Transparency Path
+        matTexTrans = Rhino.DocObjects.Material.GetTransparencyTexture(material)
+        if matTexTrans is None:
+            matTransPath.append('')
+        else:
+            matTexTransPath = Rhino.DocObjects.Texture.FileName.GetValue(matTexTrans)
+            matTransPath.append(matTexTransPath)
 
 
 with open(file_to_open, 'wb') as csv_file:
